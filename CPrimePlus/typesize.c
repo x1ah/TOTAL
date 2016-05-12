7e4d51a0426b6d71311303484903568fed0a9736
xiahei <987513441@qq.com>
2016-05-12 16:49:01 +0800

    add something

---
+++ CPrimePlus/typesize.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void)
+{
+    printf ("Type int has a size of %lu bytes.\n", sizeof (int));
+    printf ("Type char has a size of %lu bytes.\n", sizeof (char));
+    printf ("Type long has a size of %lu bytes.\n", sizeof (long));
+    printf ("Type double has a size of %lu bytes.\n", sizeof (double));
+    return 0;
+}
