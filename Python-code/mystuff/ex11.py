ae7bb21d2a700522fcfcd5b833e83586ebd48a0e
xiahei <987513441@qq.com>
2016-05-15 00:04:59 +0800

    update

--- Python-code/mystuff/ex11.py
+++
@@ -1,7 +0,0 @@
-print "how old are you ?",
-age = raw_input()
-print "how tall are you ?",
-tall = raw_input()
-print "how much do you weight?",
-weight = raw_input()
-print "so,you are %r old, %r tall and %r heavy." % (age, tall, weight)
