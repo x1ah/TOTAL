852dfb7f4c8fec2f0dcf6e567a50e944132697ed
xiahei <987513441@qq.com>
2016-05-15 13:14:07 +0800

    update

--- CPrimerPlus/wheat.c
+++ CPrimerPlus/wheat.c
@@ -4,20 +4,21 @@
 int main (void)
 {
     double current, total;
-    int count = 1;
+    int count = 'A';
 
     printf ("square     grains       total   ");
     printf ("fraction of  \n");
     printf ("   added   grain   ");
-    printf ("US total \n");
+    printf ("US total %lu  \n", sizeof ('a'));
     total = current = 1.0;
-    printf ("%4d %13.2e %12.2e %12.2e\n", count, current, total, total/CROP);
-    while (count < SQUARES)
+//    printf ("%4d %13.2e %12.2e %12.2e\n", count, current, total, total/CROP);
+    while (++count < 10)//SQUARES)
     {
-        count += 1;
+        printf ("count : %4d\n", count);
         current = 2.0 * current;
         total += current;
-        printf ("%4d %13.2e %12.2e %12.2e\n", count, current, total, total/CROP);
+        //printf ("%4d %13.2e %12.2e %12.2e\n", count, current, total, total/CROP);
     }
     printf ("That's all.\n");
+    printf ("final count: %d\n", count);
 }
