cf989213a9301294f601af30ce46b9e4c941e73f
xiahei <987513441@qq.com>
2016-05-16 16:10:48 +0800

    update

---
+++ CPrimerPlus/exercise/six/temp.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (void)
+{
+    char s;
+    while (1)
+    {
+        printf ("input the s: ");
+        scanf ("%c", &s);
+        printf ("s = %c\n", s);
+    }
+    return 0;
+}
