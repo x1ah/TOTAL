f177b90507d09f54c1200ca416ca4c22c7ee7a6b
xiahei <987513441@qq.com>
2016-05-18 21:07:11 +0800

    update

--- CPrimerPlus/exercise/six/one.c
+++ CPrimerPlus/exercise/six/one.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+int main (void)
+{
+    char letters[26];
+    char root = 'a';
+    int i;
+
+    for (i = 0; i < 26; i++)
+    {
+        letters[i] = root + i;
+        printf ("%2c", letters[i]);
+    }
+    printf ("\n");
+    return 0;
+}
