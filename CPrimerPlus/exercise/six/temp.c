5de345b0197386cbefab48f9ad71fb24f4ca3d15
xiahei <987513441@qq.com>
2016-05-18 15:29:19 +0800

    update

--- CPrimerPlus/exercise/six/temp.c
+++ CPrimerPlus/exercise/six/temp.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main (void)
 {
     char s;
-    while (1)
+    bool status = true;
+    printf ("%d\n", status);
+    while (status)
     {
         printf ("input the s: ");
         scanf ("%c", &s);
         printf ("s = %c\n", s);
+        printf ("status: %d\n", s==status);
     }
     return 0;
 }
