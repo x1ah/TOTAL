5fa1280c09d83b33f0d9b7040f0ce050800d758a
xiahei <987513441@qq.com>
2016-05-19 18:43:19 +0800

    update;

---
+++ CPrimerPlus/exercise/six/fifth.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (void)
+{
+    int start, stop, i;
+    char temp;
+
+    printf ("Input the start and stop at: ");
+    while (scanf ("%d %d", &start, &stop) == 2 || start <= stop)
+    {
+        for (i = start; i <= stop; i++)
+            printf ("the num is %d, and square is %d cube is %d\n",
+                    i, i*i, i*i*i);
+        printf ("Input the start and stop at: ");
+    }
+    return 0;
+}
