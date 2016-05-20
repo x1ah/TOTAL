95e7d7a8945c2e7189961f3a7a5698296e55386c
xiahei <987513441@qq.com>
2016-05-20 23:25:21 +0800

    update

--- CPrimerPlus/exercise/six/twelve.c
+++ CPrimerPlus/exercise/six/twelve.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+int main (void)
+{
+    int array[8];
+    int index, temp, two;
+
+    for (index = 0, temp = 1, two = 1; index < 8; index++)
+    {
+        array[index] = temp * two;
+        temp = array[index];
+        two = 2;
+    }
+    index = 0;
+    do{
+        printf ("%4d", array[index]);
+        index++;
+    } while (index < 8);
+    printf ("\n");
+    return 0;
+}
