8046a7451ef1b6113200c719c6599fe7b1f8e2af
xiahei <987513441@qq.com>
2016-05-20 21:41:52 +0800

    update

--- CPrimerPlus/exercise/six/sixth.c
+++ CPrimerPlus/exercise/six/sixth.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+int main (void)
+{
+    char words[40];
+    int index = 0;
+    int length;
+
+    printf ("Input the words: ");
+    while (scanf ("%s", words) == 1)
+    {
+        length = strlen (words);
+        printf ("the length of %s is %d\n", words, length);
+        for (index = length; index >= 0; index--)
+            printf ("%c", words[index]);
+        printf ("\n");
+        printf ("Input the words: ");
+    }
+    return 0;
+}
