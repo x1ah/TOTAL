a4170c85388af5e14ee28c17721bf591a7e4041e
xiahei <987513441@qq.com>
2016-05-21 00:25:05 +0800

    update

--- CPrimerPlus/exercise/six/fourteen.c
+++ CPrimerPlus/exercise/six/fourteen.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+int main (void)
+{
+    char words[256];
+    int index=0;
+    int length;
+
+    printf ("Input your words: ");
+    while ((words[index] = getchar()) != '\n')
+        index++;
+    length =  strlen (words);
+    for (index = length-3; index >= 0; index--)
+        printf ("%c", words[index]);
+    printf ("\n");
+    return 0;
+}
