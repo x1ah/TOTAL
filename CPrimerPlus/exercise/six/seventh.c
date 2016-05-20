03a981b81968f6f54e9c9c6962ade8ca904ba8c6
xiahei <987513441@qq.com>
2016-05-20 22:07:44 +0800

    update

--- CPrimerPlus/exercise/six/seventh.c
+++ CPrimerPlus/exercise/six/seventh.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+int main (void)
+{
+    double a, b, temp;
+
+    printf ("Input two float number: ");
+    while (scanf ("%lf %lf", &a, &b) == 2)
+    {
+        printf ("the two float num is %.4lf and %.4lf\n", a, b);
+        temp = (double) (a - b) / (a * b);
+        printf ("result is : %lf\n", temp);
+        printf ("Input two float number: ");
+    }
+    return 0;
+}
