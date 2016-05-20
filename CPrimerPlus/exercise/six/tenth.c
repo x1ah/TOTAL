1e4376dee6f81cce86e2850b5f97cdc78b3b6322
xiahei <987513441@qq.com>
2016-05-20 22:44:29 +0800

    update

--- CPrimerPlus/exercise/six/tenth.c
+++ CPrimerPlus/exercise/six/tenth.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+int main (void)
+{
+    int nums[8];
+    int index;
+
+    printf ("Input eitht num: ");
+    for (index = 0; index < 8; index++)
+        if (scanf ("%d", &nums[index]) != 1)
+        {
+            printf ("Input error");
+            return 0;
+        }
+    for (index = 7; index >= 0; index--)
+        printf("%d", nums[index]);
+    printf ("\nDone.\n");
+    return 0;
+
+}
