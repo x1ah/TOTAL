f400a1073b09eb8fcb30f2a00b1e18fbc1cabe78
xiahei <987513441@qq.com>
2016-05-20 23:38:35 +0800

    update

--- CPrimerPlus/exercise/six/thirteen.c
+++ CPrimerPlus/exercise/six/thirteen.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+#include <iso646.h>
+int main (void)
+{
+    double array_one[8];
+    double array_two[8];
+    int index;
+    double temp;
+
+    printf ("Input eight float num: ");
+    for (index = 0, temp=0; index < 8; index ++)
+    {
+        if (scanf ("%lf", &array_one[index]) != 1)
+        {
+            printf ("Input error...\n");
+            return 0;
+        }
+        array_two[index] = temp + array_one[index];
+        temp = array_two[index];
+    }
+    printf ("res is: ");
+    for (index = 0; index < 8; index++)
+        printf ("%-10f", array_two[index]);
+    printf ("\n");
+    return 0;
+}
