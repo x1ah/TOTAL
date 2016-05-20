e0cb93643f704aad5b36e6f599ffb2e512c76279
xiahei <987513441@qq.com>
2016-05-20 22:21:46 +0800

    update

--- CPrimerPlus/exercise/six/eighth.c
+++ CPrimerPlus/exercise/six/eighth.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+double cal (double a, double b);
+int main (void)
+{
+    double a, b, temp;
+
+    printf ("Input two float number[q to quit]: ");
+    while (scanf ("%lf %lf", &a, &b) == 2)
+    {
+        printf ("the two float num is %.4lf and %.4lf\n", a, b);
+        temp = cal (a, b);
+        printf ("result is : %lf\n", temp);
+        printf ("Input two float number[q to quit]: ");
+    }
+    return 0;
+}
+
+double cal (double a, double b)
+{
+    double res;
+
+    res = (a - b) / (a * b);
+    return res;
+}
