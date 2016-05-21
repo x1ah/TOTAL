bf6749121b8cf917db379270ab5ce25fe9f1578c
xiahei <987513441@qq.com>
2016-05-21 11:33:29 +0800

    update;

--- CPrimerPlus/chseven/chcount.c
+++ CPrimerPlus/chseven/chcount.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+#define PERIOD '.'
+int main (void)
+{
+    int ch;
+    int charcount = 0;
+
+    while ((ch = getchar()) != PERIOD)
+    {
+        if (ch != '"' && ch != '\'')
+            charcount++;
+    }
+    printf ("There are %d non-qote characters.\n", charcount);
+    return 0;
+}
