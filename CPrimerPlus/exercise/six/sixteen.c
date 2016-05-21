76bf1e86be610c6ab0f8f122a47689b565e1863e
xiahei <987513441@qq.com>
2016-05-21 08:59:47 +0800

    complete chapter six

--- CPrimerPlus/exercise/six/sixteen.c
+++ CPrimerPlus/exercise/six/sixteen.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+int main (void)
+{
+    double balance = 100;
+    int year = 0;
+
+    printf ("origin balance is $100\n");
+    do
+    {
+        balance += 100 * 0.08;
+        balance -= 10;
+        year++;
+    } while (balance >= 0);
+    printf ("After %d years, balance left over $%lf\n", year, balance);
+    return 0;
+}
