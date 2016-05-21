b28d498002128c694ed367682632a8a17f4fa9fb
xiahei <987513441@qq.com>
2016-05-21 12:01:05 +0800

    update

--- CPrimerPlus/chseven/wordcnt.c
+++ CPrimerPlus/chseven/wordcnt.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#define STOP '|'
+int main (void)
+{
+    char c;         //读入字符
+    char prev;      //爱你一个读入字符
+    long n_chars = 0L;  //字符数
+    int n_lines = 0;       //行数
+    int n_words = 0;    //单词数
+    int p_lines = 0;    //不完整的行数
+    bool inword = false;    //如果c在一个单词中，则inword等于true
+
+    printf ("Enter text to be analyzed (| to terminate): \n");
+    prev = '\n';
+
+    while ((c = getchar()) != STOP)
+    {
+        n_chars++;
+        if (c == '\n')
+            n_lines++;
+        if (!isspace (c) && !inword)
+        {
+             inword = true;
+             n_words++;
+        }
+        if (isspace (c) && inword)
+            inword = false;
+        prev = c;
+    }
+    if (prev != '\n')
+        p_lines = 1;
+    printf ("characters = %ld, words = %d, lines = %d, ",
+            n_chars, n_words, n_lines);
+    printf ("partial lines = %d\n", p_lines);
+    return 0;
+}
