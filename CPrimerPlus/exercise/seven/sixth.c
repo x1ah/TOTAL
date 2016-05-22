f3eb2f894cf1c333fc026c415f916cce57b69e23
xiahei <987513441@qq.com>
2016-05-22 10:03:56 +0800

    update

--- CPrimerPlus/exercise/seven/sixth.c
+++ CPrimerPlus/exercise/seven/sixth.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+int main (void)
+{
+    char words[255];
+    int index = 0;
+    int i;
+    int count = 0;
+
+    printf ("Input the words: ");
+    while ((scanf ("%c", &words[index]) == 1) && words[index] != '#')
+    {
+        if ((index >= 2) && (words[index-1] == 'e') && (words[index] == 'i'))
+        {
+            count++;
+        }
+        index++;
+    }
+    printf ("the 'ei' occer %d times\n", count);
+    return 0;
+}
