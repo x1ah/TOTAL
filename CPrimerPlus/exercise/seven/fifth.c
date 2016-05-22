e8d67a42e26b49d2f9dbc453a9bb8f7dc37bab11
xiahei <987513441@qq.com>
2016-05-22 09:31:37 +0800

    update

--- CPrimerPlus/exercise/seven/fifth.c
+++ CPrimerPlus/exercise/seven/fifth.c
@@ -1,1 +1,43 @@
 #include <stdio.h>
+int main (void)
+{
+    int n_even = 0;
+    int n_odd = 0;
+    int even_sum = 0;
+    int odd_sum = 0;
+    int num;
+    float even_avrag, odd_avrag;
+
+    printf ("Input some nums 0 to quit: ");
+    while (scanf ("%d", &num) == 1 && num != 0)
+    {
+        switch (num % 2)
+        {
+            case 0:
+                n_even++;
+                even_sum += num;
+                break;
+            case 1:
+                n_odd++;
+                odd_sum += num;
+                break;
+            default:
+                break;
+        }
+//        if (num % 2 == 0)
+//        {
+//            n_even++;
+//            even_sum += num;
+//        }
+//        else
+//        {
+//            n_odd++;
+//            odd_sum += num;
+//        }
+    }
+    even_avrag = (float) even_sum / n_even;
+    odd_avrag = (float) odd_sum / n_odd;
+    printf ("%d even number and average is %f\n%d odd number and average is %f\n",
+            n_even, even_avrag, n_odd, odd_avrag);
+    return 0;
+}
