f7d3bd6072d58bba72a6027c19ccbd7a08c15df2
xiahei <987513441@qq.com>
2016-05-22 09:24:32 +0800

    update

--- CPrimerPlus/exercise/seven/fourth.c
+++ CPrimerPlus/exercise/seven/fourth.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+int main (void)
+{
+char ch[255];
+int index = 0;
+int i;
+
+printf ("Input words: ");
+while ((scanf ("%c", &ch[index]) == 1) && ch[index] != '#')
+{
+    if (ch[index] == '.')
+        ch[index] = '!';
+    else if (ch[index] == '!')
+    {
+        ++index;
+        ch[index] = '!';
+    }
+    index++;
+}
+
+for (i = 0; i < index; i++)
+    printf ("%c", ch[i]);
+printf ("\n");
+return 0;
+}
