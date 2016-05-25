9ebbf842278ca373c04bf72f596b9cdc90ba26c9
xiahei <987513441@qq.com>
2016-05-25 23:08:57 +0800

    update

--- CPrimerPlus/exercise/eight/fourth.c
+++ CPrimerPlus/exercise/eight/fourth.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
 int main (void)
 {
     char ch;
     int allWordsLen = 0;
     int words = 0;
+    bool inword = false;
+
 
     while ((ch = getchar ()) != EOF)
     {
         if (((int) ch > 64) && ((int) ch < 123))
             ++allWordsLen;
-        else
+        if (!isspace (ch) && !inword)
+        {
+            inword = true;
             ++words;
+        }
+        if (isspace (ch) && inword)
+            inword = false;
     }
     printf ("total %d words and %d length.\n", words, allWordsLen);
     printf ("average %f charpital.\n", (float) allWordsLen / words);
