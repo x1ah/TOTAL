e4867338b17f5ced15375d40e679be6f8659b6ce
xiahei <987513441@qq.com>
2016-05-25 22:44:25 +0800

    update

--- CPrimerPlus/exercise/eight/fourth.c
+++ CPrimerPlus/exercise/eight/fourth.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+int main (void)
+{
+    char ch;
+    int allWordsLen = 0;
+    int words = 0;
+
+    while ((ch = getchar ()) != EOF)
+    {
+        if (((int) ch > 64) && ((int) ch < 123))
+            ++allWordsLen;
+        else
+            ++words;
+    }
+    printf ("total %d words and %d length.\n", words, allWordsLen);
+    printf ("average %f charpital.\n", (float) allWordsLen / words);
+    return 0;
+}
