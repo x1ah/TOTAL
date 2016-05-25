dccc736b631f9b4a0ac62151f6865fc46a6cfad7
xiahei <987513441@qq.com>
2016-05-25 18:02:33 +0800

    update

--- CPrimerPlus/exercise/eight/third.c
+++ CPrimerPlus/exercise/eight/third.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+int main (void)
+{
+    char ch;
+    int upper = 0;
+    int lower = 0;
+
+    while ((ch = getchar ()) != EOF)
+    {
+        if (((int) ch > 64) && ((int) ch < 91))
+            ++upper;
+        else if (((int) ch > 96) && ((int) ch < 123))
+            ++lower;
+        else
+            continue;
+    }
+    printf ("%d upper charpital, %d lower charpital.\n", upper, lower);
+    return 0;
+}
