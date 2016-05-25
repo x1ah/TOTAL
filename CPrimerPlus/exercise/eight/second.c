f520f243bf2968034554ea5601f7a6762e6fc9cd
xiahei <987513441@qq.com>
2016-05-25 16:21:01 +0800

    update

--- CPrimerPlus/exercise/eight/second.c
+++ CPrimerPlus/exercise/eight/second.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+int main (void)
+{
+    char ch;
+    int count = 0;
+
+    while ((ch = getchar ()) != EOF)
+    {
+        putchar ((ch == '\n') ? '\\': ch);
+        if (((int) ch) < 32)
+            putchar(ch);
+        else
+            printf ("%-5d", ch);
+        ++count;
+        if (count >= 10)
+        {
+            putchar ('\n');
+            count = 0;
+        }
+    }
+//    putchar ('\n');
+    return 0;
+}
