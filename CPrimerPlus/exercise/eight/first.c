7ecee99dd89dcc13bcc00a568749de62973f3d01
xiahei <987513441@qq.com>
2016-05-25 15:51:34 +0800

    update

--- CPrimerPlus/exercise/eight/first.c
+++ CPrimerPlus/exercise/eight/first.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+int main (void)
+{
+    int count = 0;
+    char ch;
+
+    while ((ch = getchar ()) != EOF)
+        count++;
+    printf ("Total %d charpital.\n", count);
+    printf ("END!\n");
+    return 0;
+}
