f9e7d3110eb62915e3361f995fce945fb784b6d5
xiahei <987513441@qq.com>
2016-05-26 22:47:38 +0800

    update

--- CPrimerPlus/exercise/eight/sixth.c
+++ CPrimerPlus/exercise/eight/sixth.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
+char get_first (void);
+
+int main (void)
+{
+    char ch;
+
+    ch = get_first ();
+    putchar (ch);
+    putchar ('\n');
+    return 0;
+}
+
+char get_first (void)
+{
+    char ch;
+
+    while ((ch = getchar ()) != '\n')
+    {
+        if (!isspace(ch))
+            break;
+    }
+    return ch;
+}
