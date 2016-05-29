217801f14d8c3f483e042e57cc3f7c7af935e898
xiahei <987513441@qq.com>
2016-05-29 22:34:02 +0800

    update

--- CPrimerPlus/exercise/nine/fourth.c
+++ CPrimerPlus/exercise/nine/fourth.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+double tuneAverage (double x, double y);
+int main (void)
+{
+    double res;
+
+    res = tuneAverage (10.23, 3.21);
+    printf ("res is %lf\n", res);
+    return 0;
+}
+
+double tuneAverage (double x, double y)
+{
+    double cutdownTemp, res;
+
+    cutdownTemp = (10/x + 10/y) / 2;
+    res = 1 / cutdownTemp;
+    return res;
+}
