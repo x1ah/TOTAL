28dd57545ad969dc2590091717392ee4bd4ae7c5
xiahei <987513441@qq.com>
2016-05-29 22:39:03 +0800

    update

--- CPrimerPlus/exercise/nine/fifth.c
+++ CPrimerPlus/exercise/nine/fifth.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+void larger_of (double x, double y);
+int main (void)
+{
+    larger_of (1.2, 4.2);
+    return 0;
+}
+
+void larger_of (double x, double y)
+{
+    double temp;
+
+    temp = (x > y)? x: y;
+
+    x = temp;
+    y = temp;
+    printf ("x=%lf, y=%lf\n", x, y);
+}
