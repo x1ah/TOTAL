9749ed4862dbeb6f09216ae82237182f8627e2f7
xiahei <987513441@qq.com>
2016-05-29 22:55:39 +0800

    update

--- CPrimerPlus/exercise/nine/sixth.c
+++ CPrimerPlus/exercise/nine/sixth.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+#include <ctype.h>
+int charnum (char ch);
+int main (void)
+{
+    char ch;
+
+    while ((ch = getchar ()) != EOF)
+    {
+        if (ch == '\n')
+            putchar (ch);
+        else
+            printf ("%3d", charnum (ch));
+    }
+    return 0;
+}
+
+int charnum (char ch)
+{
+    if (isalpha (ch))
+        return (((int) tolower(ch)) - 96);
+    else
+        return -1;
+}
