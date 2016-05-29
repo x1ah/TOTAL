d995c2c97a7695dac511f71d9c82f0281b128b2b
xiahei <987513441@qq.com>
2016-05-29 22:21:41 +0800

    update

--- CPrimerPlus/exercise/nine/second.c
+++ CPrimerPlus/exercise/nine/second.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+void chline (char ch, int i, int j);
+int main (void)
+{
+    chline ('+', 5, 10);
+}
+
+void chline (char ch, int i, int j)
+{
+    int temp;
+    int ch_temp;
+
+    for (temp=i; temp<j; temp++)
+    {
+        for (ch_temp=1; ch_temp<=temp; ch_temp++)
+            putchar (ch);
+        putchar ('\n');
+    }
+}
