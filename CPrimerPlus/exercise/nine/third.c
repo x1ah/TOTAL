fdc1622f154eb4a6b9e90c177a78caf3e0f2c6b6
xiahei <987513441@qq.com>
2016-05-29 22:26:47 +0800

    udpate

--- CPrimerPlus/exercise/nine/third.c
+++ CPrimerPlus/exercise/nine/third.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+void put_char (char ch, int nums, int lines);
+int main (void)
+{
+    put_char ('+', 10, 10);
+    return 0;
+}
+
+void put_char (char ch, int nums, int lines)
+{
+    int line, num;
+
+    for (line=1; line<lines; line++)
+    {
+        for (num=1; num<nums; num++)
+            putchar (ch);
+        putchar ('\n');
+    }
+}
