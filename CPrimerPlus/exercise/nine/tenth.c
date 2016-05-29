cb86287176cca48cba3c144c217b269bf8eeda6d
xiahei <987513441@qq.com>
2016-05-30 00:17:07 +0800

    update

--- CPrimerPlus/exercise/nine/tenth.c
+++ CPrimerPlus/exercise/nine/tenth.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+long Fibonacci (unsigned long n);
+int main (void)
+{
+    int n;
+    while (scanf ("%d", &n) == 1)
+        printf ("res: %ld\n", Fibonacci (n));
+    return 0;
+}
+
+long Fibonacci (unsigned long n)
+{
+    long res = 0;
+    int temp = 1;
+    int temp2 = 1;
+    int temp1 = 1;
+
+    if (n <= 2)
+        res = 1;
+    else
+        while (temp < n-1)
+        {
+            res = temp2 + temp1;
+            temp2 = temp1;
+            temp1 = res;
+            temp++;
+        }
+    return res;
+}
