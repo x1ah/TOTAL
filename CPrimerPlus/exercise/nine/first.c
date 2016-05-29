ce9e8ed1211b4cc3e7d0c40a068f70d7d898c8b6
xiahei <987513441@qq.com>
2016-05-29 22:08:29 +0800

    add second

--- CPrimerPlus/exercise/nine/first.c
+++ CPrimerPlus/exercise/nine/first.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+double min (double x, double y);
+int main (void)
+{
+    double x, y;
+    while (scanf ("%lf %lf", &x, &y) == 2)
+        printf ("The min one is %lf\n", min(x, y));
+    return 0;
+}
+
+double min (double x, double y)
+{
+    return (x >= y)? y: x;
+}
