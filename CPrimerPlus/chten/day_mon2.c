614408eeb25cd942ce5ffa20aeada7699037a111
xiahei <987513441@qq.com>
2016-05-31 22:57:56 +0800

    update

--- CPrimerPlus/chten/day_mon2.c
+++ CPrimerPlus/chten/day_mon2.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+int main (void)
+{
+    int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31};
+
+    printf ("sizeof (days) = %zd.\n", sizeof (days));
+    printf ("len (days) = %lu.\n", sizeof (days) / sizeof (days[0]));
+    return 0;
+}
