ff7fac5db171450fccc413c755bbac4565e902e4
xiahei <987513441@qq.com>
2016-06-07 21:45:04 +0800

    update

--- CPrimerPlus/chten/day_mon2.c
+++ CPrimerPlus/chten/day_mon2.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+void cat (int * day, int n);
 int main (void)
 {
     int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31};
 
     printf ("sizeof (days) = %zd.\n", sizeof (days));
     printf ("len (days) = %lu.\n", sizeof (days) / sizeof (days[0]));
+    cat (days, 10);
     return 0;
+}
+void cat (int * day, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf ("a[%d] = %d\n", i, *(day+i));
 }
