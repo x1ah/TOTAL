375cec9a3abd4b8c07dd15347c825cf1f3eb2987
xiahei <987513441@qq.com>
2016-06-09 22:58:10 +0800

    update

--- CPrimerPlus/exercise/ten/first.c
+++ CPrimerPlus/exercise/ten/first.c
@@ -3,7 +3,7 @@
 #define YEARS 5
 int main (void)
 {
-    float rain[YEARS][MONTHS] = {
+    const float rain[YEARS][MONTHS] = {
     {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
     {8.5, 8.2, 1.2, 1.6, 2.4, 0.0, 5.2, 0.9, 0.3, 0.9, 1.4, 7.3},
     {9.1, 8.5, 6.7, 4.3, 2.1, 0.8, 0.2, 0.2, 1.1, 2.3, 6.1, 8.4},
@@ -11,7 +11,7 @@ int main (void)
     {7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2}
     };
     int year, month;
-    float (* pt)[MONTHS];
+    const float (* pt)[MONTHS];
     pt = rain;
 
     float subtot, total;
--- CPrimerPlus/exercise/ten/second.c
+++ CPrimerPlus/exercise/ten/second.c
@@ -1,1 +1,40 @@
 #include <stdio.h>
+void copy_arr (double * source, double * target1, int len);
+void copy_ptr (double * source, double * target2, int len);
+int main (void)
+{
+    double source [5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    double target1[5];
+    double target2[5];
+
+    copy_arr (source, target1, 5);
+    copy_ptr (source, target2, 5);
+    return 0;
+}
+
+void copy_arr (double * source, double * target1, int len)
+{
+    int temp = 0;
+
+    printf ("target1 = {");
+    for (temp = 0; temp < len; temp ++)
+    {
+        target1[temp] = source[temp];
+        printf ("%4.1f,", target1[temp]);
+    }
+    printf ("}\n");
+}
+
+void copy_ptr (double * source, double * target2, int len)
+{
+    int temp = 0;
+    double te;
+    printf ("target2 = {");
+    for (temp = 0; temp < len; temp ++)
+    {
+        te = * (source + temp);
+        * (target2 + temp) = te;
+        printf ("%4.1f,", te);
+    }
+    printf ("}\n");
+}
