c1630f2c1d87700567f96b9243a06e556437e793
xiahei <987513441@qq.com>
2016-06-10 16:44:55 +0800

    update

--- CPrimerPlus/exercise/ten/fourth.c
+++ CPrimerPlus/exercise/ten/fourth.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+int max_index (double * arr, int len);
+int main (void)
+{
+    double arr[10] = {2.3, 3.4, 32.3, 5.5, 23.4, 5.34, 52.32, 54.6, 7.5, 875.5};
+    int index;
+    index = max_index (arr, 10);
+    printf ("The index of max nubmer is: %d\n", index);
+    return 0;
+}
+
+
+int max_index (double * arr, int len)
+{
+    int res = 0;
+    int index = 0;
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        index = (*(arr+i) > res)? i: index;
+        res = (*(arr+i) > res)? *(arr+i): res;
+    }
+    return index;
+}
