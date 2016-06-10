a20efd4684d53bfcbcf2bc54c02ecdb2070198f7
xiahei <987513441@qq.com>
2016-06-10 17:13:05 +0800

    update

--- CPrimerPlus/exercise/ten/fifth.c
+++ CPrimerPlus/exercise/ten/fifth.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+double minmax (double * arr, int len);
+int main (void)
+{
+    double arr[5] = {3.4, 221.4, 5.2, 4.3, 9.2};
+
+    printf ("max - min = %lf\n", minmax (arr, 5));
+    return 0;
+}
+
+double minmax (double * arr, int len)
+{
+    int i;
+    double min = * arr;
+    double max = * arr;
+    double res;
+
+    for (i = 0; i < len; i++)
+    {
+        min = (*(arr+i) > min)? min: *(arr+i);
+        max = (*(arr+i) < max)? max: *(arr+i);
+    }
+    res = max - min;
+    return res;
+}
