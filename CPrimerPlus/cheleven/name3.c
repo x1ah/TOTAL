2998dd41d455b6c0b411b1e9a2bf9931bec62eed
xiahei <987513441@qq.com>
2016-06-21 23:34:47 +0800

    update

--- CPrimerPlus/cheleven/name3.c
+++ CPrimerPlus/cheleven/name3.c
@@ -1,1 +1,13 @@
 #include <stdio.h>
+#define MAX 81
+int main (void)
+{
+    char name[MAX];
+    char * ptr;
+
+    printf ("Hi, whar's your name? \n");
+    ptr = fgets (name, MAX, stdin);
+    printf ("%s? ah! %s!", name, ptr);
+
+    return 0;
+}
