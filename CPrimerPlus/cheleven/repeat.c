263329cf0134da760099e2aecee2d0fa333d3963
xiahei <987513441@qq.com>
2016-06-22 17:30:11 +0800

    update repeat

--- CPrimerPlus/cheleven/repeat.c
+++ CPrimerPlus/cheleven/repeat.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main (int argc, char * argv[])
-{ int count;
+{
+    int count;
 
     printf ("The command line has %d arguments: \n", argc-1);
     for (count = 1; count < argc; count ++)
