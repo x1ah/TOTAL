4b983f7639bcd110a040c5288bdd860c311dff1b
xiahei <987513441@qq.com>
2016-06-22 22:55:55 +0800

    update

--- CPrimerPlus/exercise/eleven/second.c
+++ CPrimerPlus/exercise/eleven/second.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+int getArray (char * ar, int len);
+int main (void)
+{
+    int len = 20;
+    char arr[25];
+
+    getArray(arr, len);
+    fputs (arr, stdout);
+    putchar ('\n');
+    return 0;
+}
+
+int getArray (char * ar, int len)
+{
+    int i;
+    char temp;
+    printf ("Input your string longer than 20: \n");
+
+    fgets (ar, 81, stdin);
+    for (i=len; i < 81; i ++)
+    {
+        temp = *(ar+i);
+        if (temp == ' ' || temp == '\n' || temp == '\t')
+        {
+            *(ar+i) = '\0';
+            return 0;
+        }
+    }
+}
