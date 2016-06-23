8ddda9a50e8c5245e9157f031d2bfc0d3520d4c9
xiahei <987513441@qq.com>
2016-06-23 23:29:38 +0800

    update

--- CPrimerPlus/exercise/eleven/ninth.c
+++ CPrimerPlus/exercise/eleven/ninth.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+char * strip (char * str);
+int main (void)
+{
+    char str[81];
+
+    do
+    {
+        printf ("\nInput some words: \n");
+        fgets (str, 81, stdin);
+        strip (str);
+        fputs (str, stdout);
+    } while (* str != '\n');
+
+    return 0;
+}
+
+char * strip (char * str)
+{
+    int index1, index2;
+    int len_str = (int) (strlen (str));
+
+    for (index1 = index2 = 0; index1 < len_str; index1 ++)
+    {
+        if (*(str + index1) != ' ')
+            *(str + index2++) = *(str + index1);
+    }
+    *(str + index2) = '\0';
+    return str;
+}
