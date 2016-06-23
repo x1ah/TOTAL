7747d430170e1b54de9b3d266271f2d0b81f6b5d
xiahei <987513441@qq.com>
2016-06-23 22:46:53 +0800

    update

--- CPrimerPlus/exercise/eleven/eighth.c
+++ CPrimerPlus/exercise/eleven/eighth.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+char * reverse (char * str);
+int main (void)
+{
+    char str[] = "hello world.";
+
+    reverse (str);
+    printf ("after reverse: \n");
+    fputs (str, stdout);
+
+    return 0;
+}
+
+char * reverse (char * str)
+{
+    int len = (int) (strlen (str));
+    char temp[len];
+    int i;
+    strncpy (temp, str, len);
+
+    for (i = 0; i < len; i ++)
+        *(str+i) = *(temp+len-i-1);
+
+    return str;
+}
