479e75b793f36de77bef93c90d15e5239cc1d30d
xiahei <987513441@qq.com>
2016-06-23 09:30:38 +0800

    update

--- CPrimerPlus/exercise/eleven/third.c
+++ CPrimerPlus/exercise/eleven/third.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+char * getArray (char * ar);
+int main (void)
+{
+    char arr[20];
+
+    getArray(arr);
+    printf ("\nAfter split: \n");
+    fputs (arr, stdout);
+    putchar ('\n');
+}
+
+char * getArray (char * ar)
+{
+    int i;
+    char temp;
+    printf ("Input your string: \n");
+
+    fgets (ar, 81, stdin);
+    for (i=0; ;i ++)
+    {
+        temp = *(ar+i);
+        if (temp == ' ' || temp == '\n' || temp == '\t')
+        {
+            *(ar+i) = '\0';
+            return ar;
+        }
+    }
+}
