0192c21855df0a17862d0a06ec59b0dfab5763e5
xiahei <987513441@qq.com>
2016-06-23 23:52:46 +0800

    update

--- CPrimerPlus/exercise/eleven/eleven.c
+++ CPrimerPlus/exercise/eleven/eleven.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
+int main (void)
+{
+    char ch;
+    int words = 0;
+    int up_words = 0;
+    int low_words = 0;
+    int punctions = 0;
+    int nums = 0;
+
+    while ((ch = getchar ()) != EOF)
+    {
+        if (isspace (ch))
+            words++;
+        if (islower (ch))
+            low_words++;
+        if (isupper (ch))
+            up_words ++;
+        if (isdigit(ch))
+            nums ++;
+        if (ispunct (ch))
+            punctions ++;
+    }
+    printf ("\n%d words, %d upper chars, %d lower words, %d punctions, %d numbers\n",
+            words, up_words, low_words, punctions, nums);
+    return 0;
+}
