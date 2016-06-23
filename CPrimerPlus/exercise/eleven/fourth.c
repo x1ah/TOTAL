553979b6af12c7b380d53589f6cb26a59e77f71c
xiahei <987513441@qq.com>
2016-06-23 09:49:11 +0800

    update

--- CPrimerPlus/exercise/eleven/fourth.c
+++ CPrimerPlus/exercise/eleven/fourth.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+char * search (char * source_string, char ch);
+int main (void)
+{
+    char * tar;
+
+    char str[] = "hello Python and World.";
+    tar = search (str, 'P');
+    fputs (tar, stdout);
+    return 0;
+}
+
+char * search (char * source_string, char ch)
+{
+    int index;
+    char * temp_char;
+
+    for (index = 0; ;index ++)
+    {
+        temp_char = source_string + index;
+        if (* temp_char == ch)
+            return temp_char;
+        else if (*temp_char == '\0')
+            return temp_char;
+        else
+            ;
+    }
+}
