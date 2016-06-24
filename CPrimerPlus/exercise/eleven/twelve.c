98c51bb7f920abf7af6b7a87e8d43fad514e3aa9
xiahei <987513441@qq.com>
2016-06-24 10:23:52 +0800

    update

--- CPrimerPlus/exercise/eleven/twelve.c
+++ CPrimerPlus/exercise/eleven/twelve.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+int main (int argc, char *argv[])
+{
+    int count;
+
+    for (count=argc-1; count > 0; count--)
+        printf ("%s ", *(argv+ count));
+    printf ("\n");
+
+    return 0;
+}
