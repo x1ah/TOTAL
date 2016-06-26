d46265b10567597ccac958e53c16312ba3572c86
xiahei <987513441@qq.com>
2016-06-26 16:17:41 +0800

    update

--- CPrimerPlus/chtwelve/r_drive0.c
+++ CPrimerPlus/chtwelve/r_drive0.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+extern int rand0 (void);
+
+int main (void)
+{
+    int count;
+
+    for (count = 0; count < 5; count ++)
+        printf ("%d\n", rand0 ());
+
+    return 0;
+}
--- CPrimerPlus/chtwelve/rand0.c
+++ CPrimerPlus/chtwelve/rand0.c
@@ -1,1 +1,6 @@
-#include <stdio.h>
+static unsigned long int next = 1;
+int rand0 (void)
+{
+    next = next * 110351245 + 1234;
+    return (unsigned int) (next/65536) % 32768;
+}
