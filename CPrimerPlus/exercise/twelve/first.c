b88ceba7cd0832aa05710519c27e27db9d0a3b71
xiahei <987513441@qq.com>
2016-06-26 22:22:07 +0800

    updatep

--- CPrimerPlus/exercise/twelve/first.c
+++ CPrimerPlus/exercise/twelve/first.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+void critic (int * pt);
+int main (void)
+{
+    static int units = 0;
+
+    printf ("How many pounds to a firkin of butter?\n");
+    scanf ("%d", &units);
+    while (units != 56)
+        critic (&units);
+    printf ("You must have looked it up\n");
+    return 0;
+}
+
+void critic (int * pt)
+{
+    printf ("No luck, chummy. Try again.\n");
+    scanf ("%d", pt);
+}
