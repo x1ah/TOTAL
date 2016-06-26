0c1950d55f8d965ec950b0f758a17250a9a4315f
xiahei <987513441@qq.com>
2016-06-26 23:17:58 +0800

    update

--- CPrimerPlus/exercise/twelve/pe12-2a.c
+++ CPrimerPlus/exercise/twelve/pe12-2a.c
@@ -1,1 +1,54 @@
 #include <stdio.h>
+#include "pe12-2a.h"
+static int mode = 1;
+static float distant = 0;
+static float fuel = 0;
+
+void set_mode (int modes)
+{
+    extern int mode;
+
+    if (modes > 1)
+    {
+        printf ("Invalid mode specified. Mode 1 (US) used.\n");
+        mode = 1;
+    }
+    else
+        mode = modes;
+
+}
+
+void get_info (void)
+{
+    switch (mode)
+    {
+        case 0:
+            printf ("Enter distance traveled in kilometers: ");
+            scanf ("%f", &distant);
+            printf ("Enter fuel consumed in liters: ");
+            scanf ("%f", &fuel);
+            break;
+        case 1:
+            printf ("Enter distance traveled in miles: ");
+            scanf ("%f", &distant);
+            printf ("Enter fuel consumed in gallons: ");
+            scanf ("%f", &fuel);
+            break;
+    }
+
+}
+
+void show_info (void)
+{
+    switch (mode)
+    {
+        case 0:
+            printf ("Fuel consumption is %.2f liters per 100 km.\n",
+                    fuel / distant * 100);
+            break;
+        case 1:
+            printf ("Fuel consumption is %.2f miles per galion.\n",
+                    distant / fuel);
+            break;
+    }
+}
