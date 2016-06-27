61f10892ca6939c9fe07d5f58693b976ba9acd05
xiahei <987513441@qq.com>
2016-06-27 15:55:46 +0800

    update

--- CPrimerPlus/exercise/twelve/fourth.c
+++ CPrimerPlus/exercise/twelve/fourth.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+int count (void);
+int main (void)
+{
+    int i, times;
+
+    for (i = 0; i < 10; i++)
+        times = count ();
+    printf ("call back %d times\n", times);
+    return 0;
+}
+
+int count (void)
+{
+    static int times = 0;
+    times += 1;
+    return times;
+}
