40f925820b70c8d619704813a4b2bed780b4859c
xiahei <987513441@qq.com>
2016-06-29 11:44:32 +0800

    update

--- CPrimerPlus/chthirteen/addaword.c
+++ CPrimerPlus/chthirteen/addaword.c
@@ -1,1 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#define MAX 40
+int main (void)
+{
+    FILE * fp;
+    char words[MAX];
+
+    if ((fp = fopen("words", "a+")) == NULL)
+    {
+        fprintf (stdout, "Can't open \" words \"file \n");
+        exit (1);
+    }
+    puts ("Enter words to add to the fiile: press the Enter");
+    fputs ("keys at the beginning of a line to terminate.\n", stdout);
+    while (fgets (words, MAX, stdin) && (words[0] != '\0') && *words != '\n')
+        fprintf (fp, "%s ", words);
+    fputs ("File contents: \n", stdout);
+    rewind (fp);
+    while (fscanf (fp, "%s", words) == 1)
+    {
+        fputs (words, stdout);
+        putchar ('\n');
+    }
+    if (fclose (fp) != 0)
+        fprintf (stderr, "Error closing file\n");
+    return 0;
+}
