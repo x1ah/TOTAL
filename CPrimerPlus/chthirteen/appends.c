1aeee98b640cc3b9a346b2b4ac3d03d7fd370d63
xiahei <987513441@qq.com>
2016-07-04 00:22:24 +0800

    update

--- CPrimerPlus/chthirteen/appends.c
+++ CPrimerPlus/chthirteen/appends.c
@@ -1,1 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define BUFSIZE 1024
+#define SLEN 81
+void append (FILE * source, FILE * dest);
+
+int main (void)
+{
+    FILE * fa, *fs;
+    int files = 0;
+    char file_app[SLEN];
+    char file_src[SLEN];
+    fputs ("Enter name of destination file: \n", stdout);
+    fgets (file_app, SLEN, stdin);
+
+    if (file_app[strlen (file_app) -1] == '\n')
+        file_app[strlen(file_app)-1] = '\0';
+
+    if ((fa = fopen(file_app, "a")) == NULL)
+    {
+        fprintf (stderr, "Can't open %s\n", file_app);
+        exit (2);
+    }
+    if (setvbuf (fa, NULL, _IOFBF, BUFSIZE) != 0)
+    {
+        fputs ("Can't create output buffer\n", stderr);
+        exit (3);
+    }
+    fputs ("Enter name of first source file (empty line to quit): \n", stdout);
+    while (fgets (file_src, SLEN, stdin) && file_src[0] != '\n')
+    {
+        if (file_src[strlen (file_src) -1] == '\n')
+            file_src[strlen(file_src)-1] = '\0';
+
+        if (strcmp (file_src, file_app) == 0)
+            fputs ("Can't append file to itself\n", stderr);
+        else if ((fs = fopen (file_src, "r")) == NULL)
+            fprintf (stderr, "can't open %s\n", file_src);
+        else
+        {
+            if (setvbuf (fs, NULL, _IOFBF, BUFSIZE) != 0)
+            {
+                fputs ("can't creat input buffer\n", stderr);
+                continue;
+            }
+            append (fs, fa);
+            if (ferror (fs) != 0)
+                fprintf (stderr, "Error in reading file %s.\n", file_src);
+            if (ferror (fa) != 0)
+                fprintf (stderr, "Error in writing file %s.\n", file_app);
+            fclose (fs);
+            files++;
+            printf ("File %s appended.\n", file_src);
+            fputs ("Next file (empty line to quit): \n", stdout);
+        }
+    }
+    printf ("Done .%d files appended.\n", files);
+    fclose (fa);
+    return 0;
+}
+
+void append (FILE * source, FILE * dest)
+{
+    size_t bytes;
+    static char temp[BUFSIZE];
+
+    while ((bytes = fread (temp, sizeof (char), BUFSIZE, source)) > 0)
+        fwrite (temp, sizeof (char), bytes, dest);
+}
+
