b7a23c370507af70875feb50dc338fcc72219d11
xiahei <987513441@qq.com>
2016-07-05 22:20:56 +0800

    update

---
+++ CPrimerPlus/mygets.h
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+char * mygets (char * name, int size)
+// 去除输入字符串尾的换行符
+{
+    fgets (name, size, stdin);
+    if (name[strlen (name) - 1] == '\n')
+        name[strlen (name) - 1] = '\0';
+    return name;
+}
