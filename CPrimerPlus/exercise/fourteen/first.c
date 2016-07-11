e989eb6c828dbf1c300676f3daab58f1dfe98326
xiahei <987513441@qq.com>
2016-07-11 22:41:16 +0800

    update

--- CPrimerPlus/exercise/fourteen/first.c
+++ CPrimerPlus/exercise/fourteen/first.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+struct date {
+    char month[20];
+    char mo[4];
+    int day;
+};
+
+int main (void)
+{
+    struct date test_month = {
+        "octember",
+        "OCT",
+        31
+    };
+
+    printf ("%s (%s) have %d days.\n", test_month.month, test_month.mo, test_month.day);
+    return 0;
+}
