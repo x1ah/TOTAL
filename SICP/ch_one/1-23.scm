1cdedc3da9b5b4a38ae6267382c8337d768cfe8f
xiahei <987513441@qq.com>
2016-07-17 16:21:29 +0800

    update

---
+++ SICP/ch_one/1-23.scm
@@ -0,0 +1,10 @@
+(load "1-21.scm")
+
+(define (next n)
+  (if (= n 2) 3
+    (+ n 2)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
