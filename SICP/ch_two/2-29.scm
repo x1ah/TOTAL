a76d2a42f903742599cc0acdada862b73a9f06e8
xiahei <987513441@qq.com>
2016-07-29 20:09:08 +0800

    2-29

--- SICP/ch_two/2-29.scm
+++ SICP/ch_two/2-29.scm
@@ -28,3 +28,12 @@
 (define (total-weight mobile)
   (+ (branch-weight (left-branch mobile)
                     (right-branch mobile))))
+
+;;; c)
+
+(define (branch-torque branch)
+  (* (branch-length branch) (branch-weight branch)))
+
+(define (balance-mobile? mobile)
+  (= (branch-torque (left-branch mobile))
+     (branch-torque (right-branch mobile))))
