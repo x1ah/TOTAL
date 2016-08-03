55e3a5af328d109da1874f7e5d391fee70339d2e
xiahei <987513441@qq.com>
2016-08-03 10:10:35 +0800

    2-60

--- SICP/ch_two/2-60.scm
+++ SICP/ch_two/2-60.scm
@@ -1,23 +1,22 @@
-
 (define (element-of-set? x set)
   (cond ((null? set) #f)
         ((equal? x (car set)) #t)
         (else (element-of-set? x (cdr set)))))
 
 (define (adjoin-set x set)
-  (if (element-of-set? x set) set)
   (cons x set))
 
+(define (intersection-set-iter set1 set2 res)
+  (cond ((or (null? set1) (null? set2)) res)
+        ((and (element-of-set? (car set1) set2)
+             (not (element-of-set? (car set1) res)))
+             (intersection-set-iter (cdr set1) set2 (cons (car set1) res)))
+        (else (intersection-set-iter (cdr set1) set2 res))))
+
 (define (intersection-set set1 set2)
-  (cond ((or (null? set1) (null? set2)) '())
-        ((element-of-set? (car set1) set2)
-         (cons (car set1)
-               (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
+  (intersection-set-iter set1 set2 '()))
 
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
         ((null? set2) set1)
-        ((not (element-of-set? (car set1) set2))
-         (cons (car set1) (union-set (cdr set1) set2)))
-        (else (union-set (cdr set1) set2))))
+        (else (cons (car set1) (union-set (cdr set1) set2)))))
