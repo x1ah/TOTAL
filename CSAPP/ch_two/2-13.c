461fb1ad965059cb6d622988f65537302286c2a4
xiahei <987513441@qq.com>
2016-08-10 20:33:32 +0800

    2-13.c

---
+++ CSAPP/ch_two/2-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int bis(int x, int m);
+int bic(int x, int m);
+
+int bool_or(int x, int y)
+{
+    int result = bis (x, y);
+    return result;
+}
+
+int bool_xor(int x, int y)
+{
+    int result = bis (bic (x, y), bic (y, x));
+    return result;
+}
+
+
