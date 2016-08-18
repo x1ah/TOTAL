9b464fb0c116dabbb0db4008cf77cecd7fda50b0
xiahei <987513441@qq.com>
2016-08-18 22:28:08 +0800

    2-58.c

--- CSAPP/ch_two/2-58.c
+++ CSAPP/ch_two/2-58.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+typedef unsigned char *byte_pointer;
+
+int is_little_endian(void);
+
+int main (void)
+{
+    printf ("%d\n", is_little_endian());
+    return 0;
+}
+
+int is_little_endian(void)
+{
+    int foo = 1;
+    byte_pointer res = (byte_pointer) &foo;
+    return !res[0];
+}
