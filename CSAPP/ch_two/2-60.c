dd6325f8b602f3d397ad126861b9528d0a5b5881
xiahei <987513441@qq.com>
2016-08-19 01:20:12 +0800

    2-60;

--- CSAPP/ch_two/2-60.c
+++ CSAPP/ch_two/2-60.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+ unsigned replace_byte (unsigned x, unsigned char b, int i)
+{
+    return ((x & (~(0xff << i*8))) | (b << i*8));
+}
+
+int main (void)
+{
+    printf ("%x\n", replace_byte (0x12345678, 0xab, 0));
+    return 0;
+}
