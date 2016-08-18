acd530783533b3ff3d5aa8a8ab883e6eb3ae579f
xiahei <987513441@qq.com>
2016-08-19 00:20:33 +0800

    update 2-58

--- CSAPP/ch_two/2-58.c
+++ CSAPP/ch_two/2-58.c
@@ -15,3 +15,12 @@ int is_little_endian(void)
     byte_pointer res = (byte_pointer) &foo;
     return !res[0];
 }
+
+/* the another method */
+
+int is_little_endian_(void)
+{
+    int bar = 1;
+    char *p = (char *) &bar;
+    return *p;
+}
