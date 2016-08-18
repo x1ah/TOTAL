de66031fd7b88a89106cbfc36d8a1ea205119a39
xiahei <987513441@qq.com>
2016-08-18 23:59:00 +0800

    2-59

--- CSAPP/ch_two/2-59.c
+++ CSAPP/ch_two/2-59.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+typedef unsigned char *char_pointer;
+unsigned trans (unsigned x, unsigned y);
+
+int main (void)
+{
+    unsigned x = 0x89abcdef;
+    unsigned y = 0x76543210;
+
+    printf ("%x\n", trans(x, y));
+    return 0;
+}
+
+unsigned trans (unsigned x, unsigned y)
+{
+    return (x & 0xff) | (y & ~0xff);
+}
