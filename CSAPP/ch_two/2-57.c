b4f191e2e79f19eb045e05e13c5e2476b7328bef
xiahei <987513441@qq.com>
2016-08-18 21:51:25 +0800

    2-57.c

--- CSAPP/ch_two/2-57.c
+++ CSAPP/ch_two/2-57.c
@@ -1,1 +1,36 @@
 #include <stdio.h>
+#include "show-bytes.h"
+
+void show_short(short x);
+void show_long(long x);
+void show_double(double x);
+
+int main (void)
+{
+    short short_x = 12345;
+    long long_x = 12345;
+    double double_x = 12345.0;
+
+    printf ("show_short:\n");
+    show_short (short_x);
+    printf ("show_long:\n");
+    show_long (long_x);
+    printf ("show_double:\n");
+    show_double (double_x);
+
+}
+
+void show_short(short x)
+{
+    show_bytes ((byte_pointer) &x, sizeof (short));
+}
+
+void show_long (long x)
+{
+    show_bytes ((byte_pointer) &x, sizeof (long));
+}
+
+void show_double (double x)
+{
+    show_bytes ((byte_pointer) &x, sizeof(double));
+}
