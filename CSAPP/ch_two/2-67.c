743b8b3fa534043a0e4aa901ea6cf6ad5c9babd0
xiahei <987513441@qq.com>
2016-08-19 21:28:50 +0800

    2-67

--- CSAPP/ch_two/2-67.c
+++ CSAPP/ch_two/2-67.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+int bad_int_size_is_32(void)
+{
+    int set_msb = 1 << 31;
+    int beyond_msb = (~set_msb) >> 31;
+
+    return !beyond_msb;
+}
+
+int main (void)
+{
+    printf ("%d\n", bad_int_size_is_32());
+    return 0;
+}
