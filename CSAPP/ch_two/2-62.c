77ff2335c97b6f7d7f94b7735b52a31103c0fe9e
xiahei <987513441@qq.com>
2016-08-19 15:09:25 +0800

    2-62

--- CSAPP/ch_two/2-62.c
+++ CSAPP/ch_two/2-62.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+int int_shifts_are_logical (void);
+
+int main (void)
+{
+    printf ("%d\n", int_shifts_are_logical());
+    return 0;
+}
+
+int int_shifts_are_logical (void)
+{
+    int w = sizeof (int) << 3;
+    return !(~((1 << (w-1)) >> (w-1)));
+}
