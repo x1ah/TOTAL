a3c30c9c5ea86130a2781f04b82ac8109a99a549
xiahei <987513441@qq.com>
2016-08-19 14:46:42 +0800

    2-61

--- CSAPP/ch_two/2-61.c
+++ CSAPP/ch_two/2-61.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+int func (int x);
+
+int main (void)
+{
+    int x = 0xf4123466;
+    printf ("%d\n", func(x));
+    return 0;
+}
+
+int func (int x)
+{
+    int shift_val = (sizeof (int)-1) << 3;
+ /*   printf ("%d\n", shift_val);
+    printf ("%d, %d, %d, %d\n", !(~x), !x, !(x >> shift_val ^ 0x00), !(((x << shift_val)>>shift_val) & 0xff)); */
+    return !(~x) ||
+        !x ||
+        !(x >> shift_val ^ 0x00) ||
+        !(((x << shift_val)>>shift_val) & 0xff);
+}
