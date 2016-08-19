d5c85ca5cc946f80b0cc47a08927e4aa17c0a6d1
xiahei <987513441@qq.com>
2016-08-19 15:24:47 +0800

    2-63

--- CSAPP/ch_two/2-63.c
+++ CSAPP/ch_two/2-63.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+int sra (int x, int k);
+unsigned srl (unsigned x, int k);
+
+int main (void)
+{
+    return 0;
+}
+
+int sra (int x, int k)
+{
+    int xsrl = (unsigned) x >> k;
+    int w = sizeof (int) << 3;
+    return xsrl & ((1 << (w-k))-1);
+}
+
+unsigned srl (unsigned x, int k)
+{
+    unsigned xsra = (int) x >> k;
+    int w = sizeof (int) << 3;
+    return (~xsra) ^ ((1 << (w-k))-1);
+}
