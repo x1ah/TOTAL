457307eb6d97d755b82970b9839aeedfd37f58b9
xiahei <987513441@qq.com>
2016-08-20 15:45:31 +0800

    2-70

--- CSAPP/ch_two/2-70.c
+++ CSAPP/ch_two/2-70.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+int fits_bits (int x, int n);
+
+int main (void)
+{
+    return 0;
+}
+
+int fits_bits (int x, int n)
+{
+    int max = ~(1 << (n-1));
+
+    int res = max & x;
+    return !res;
+}
