7ce65798480482961ce2cc7a51c91f4cb1734378
xiahei <987513441@qq.com>
2016-08-20 20:14:29 +0800

    2-72

--- CSAPP/ch_two/2-72.c
+++ CSAPP/ch_two/2-72.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main (void)
+/*
+ * A) int 类型和 unsigned 类型做运算，最后int转换成无符号类型，结果也是无符号，无符号数永远大于零
+ */
+
+ void copy_int (int val, void *buf, int maxbytes)
 {
-    return 0;
+    if ((maxbytes > 0) && ((unsigned) maxbytes >= sizeof (int)))
+        memcpy (buf, (void *) &val, sizeof (val));
 }
