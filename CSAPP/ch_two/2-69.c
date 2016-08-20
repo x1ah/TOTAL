51820e9248c4a19ea98ca40c24810345f2601739
xiahei <987513441@qq.com>
2016-08-20 15:12:33 +0800

    2-69

--- CSAPP/ch_two/2-69.c
+++ CSAPP/ch_two/2-69.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+unsigned rotate_right_shift (unsigned x, int n);
+
 int main (void)
 {
+    unsigned x = 0x12345678;
+    int n = 4;
+//    printf ("hello world\n");
+    printf ("0x%x\n", rotate_right_shift(x, n));
     return 0;
+}
+
+unsigned rotate_right_shift (unsigned x, int n)
+{
+    return (x >> n) | (x << (32-n));
 }
