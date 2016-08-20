68d93dd1a8c98b399579b520d30ab4b09258650c
xiahei <987513441@qq.com>
2016-08-20 20:26:28 +0800

    2-73

--- CSAPP/ch_two/2-73.c
+++ CSAPP/ch_two/2-73.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <limits.h>
+int saturating_add (int x, int y);
 
 int main (void)
 {
     return 0;
+}
+
+int saturating_add (int x, int y)
+{
+    int w = sizeof (int);
+    if (x + y < -(1<<(w-1)))
+        return INT_MIN;
+    else if (x + y >= (1<<(w-1)))
+        return INT_MAX;
+    else
+        return x + y;
 }
