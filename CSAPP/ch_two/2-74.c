fe2f4c7ccfec17a27dc92a570ffbbf72c044a337
xiahei <987513441@qq.com>
2016-08-20 20:56:12 +0800

    2-74

--- CSAPP/ch_two/2-74.c
+++ CSAPP/ch_two/2-74.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main (void)
 {
     return 0;
+}
+
+int tsuy_ovf (int x, int y)
+{
+    if ((x > 0 && y > 0 && x > INT_MAX + y)
+        || (x < 0 && y < 0 && x < INT_MIN + y))
+            return 1;
+    else
+        return x - y;
 }
