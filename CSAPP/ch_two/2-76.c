e9c3c1b53692ed2cd997c1fe7b274bba49cdb6fa
xiahei <987513441@qq.com>
2016-08-22 12:31:52 +0800

    2-76

--- CSAPP/ch_two/2-76.c
+++ CSAPP/ch_two/2-76.c
@@ -2,5 +2,11 @@
 
 int main (void)
 {
+    int x = 2;
+    int res_k_5 = (x << 2) + x;
+    int res_k_9 = (x << 3) + x;
+    int res_k_30 = (x << 5) - (x << 1);
+    int res_k_56 = (x << 3) - (x << 6);
+    printf ("%d, %d, %d, %d", res_k_5, res_k_9, res_k_30, res_k_56);
     return 0;
 }
