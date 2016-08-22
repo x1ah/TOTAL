0447c26577191268acf09f97cc10822ee7c1274a
xiahei <987513441@qq.com>
2016-08-22 13:35:31 +0800

    2-78

--- CSAPP/ch_two/2-78.c
+++ CSAPP/ch_two/2-78.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main (void)
 {
     return 0;
+}
+
+int mul5div8 (int x)
+{
+    int overflow = (((x << 2) + x) & INT_MIN) == INT_MIN;
+    return (((x << 2) + x) >> 3) & (INT_MAX + overflow);
 }
