40986684137de1c57b11db7a29031e6f172907b9
xiahei <987513441@qq.com>
2016-08-22 12:00:52 +0800

    2-75

--- CSAPP/ch_two/2-75.c
+++ CSAPP/ch_two/2-75.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+unsigned unsigned_high_prod (unsigned x, unsigned y);
 
 int main (void)
 {
+    printf ("0x%x\n", 0xfeffffee * 0xffffffee >> 16);
+    printf ("0x%x\n", 6 * 6 >> 3);
+    printf ("0x%x\n", unsigned_high_prod(0xff, 0xef));
     return 0;
+}
+
+unsigned unsigned_high_prod (unsigned x, unsigned y)
+{
+    int w = sizeof (unsigned) << 3;
+    printf ("%d\n", w);
+    return ((x * y) >> w);
 }
