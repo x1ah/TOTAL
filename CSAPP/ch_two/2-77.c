7ff879dc7e953ed7bee94edea579d7d82f25d3c3
xiahei <987513441@qq.com>
2016-08-22 12:50:09 +0800

    2-77

--- CSAPP/ch_two/2-77.c
+++ CSAPP/ch_two/2-77.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+int divide_power2 (int x, int k);
 
 int main (void)
 {
+    printf ("%d", divide_power2(16, 2));
     return 0;
+}
+
+int divide_power2 (int x, int k)
+{
+    return x >> k;
 }
