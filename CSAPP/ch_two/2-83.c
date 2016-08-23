2d9828158ec614872c240f2fb9c0d29ee8a3b657
xiahei <987513441@qq.com>
2016-08-23 13:23:27 +0800

    2-83

--- CSAPP/ch_two/2-83.c
+++ CSAPP/ch_two/2-83.c
@@ -4,3 +4,15 @@ int main (void)
 {
     return 0;
 }
+
+int float_ge (float x, float y)
+{
+    unsigned ux = f2u(x);
+    unsigned uy = f2u(y);
+
+    unsigned sx = ux >> 31;
+    unsigned sy = uy >> 31;
+
+    /* waiting resolve */
+    return (sx ^ 0 && sy & 1);
+}
