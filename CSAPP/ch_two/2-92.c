8c40ab5f8484b38ec0922a321f56c452689d6d06
xiahei <987513441@qq.com>
2016-08-23 13:42:38 +0800

    2-92

--- CSAPP/ch_two/2-92.c
+++ CSAPP/ch_two/2-92.c
@@ -7,11 +7,12 @@ int main (void)
 
 float_bits float_absval (float_bits f)
 {
+    unsigned sign = f >> 31;
     unsigned exp = f >> 23 & 0xff;
     unsigned frac = f & 0x7fffff;
 
     if (exp == 0xff && frac != 0)
         return f;
 
-    return (exp << 23) | frac;
+    return !sign | (exp << 23) | frac;
 }
